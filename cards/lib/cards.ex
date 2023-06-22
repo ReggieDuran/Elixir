68f1bf7af9761e396f11d5d0341e99b385290077
Reggie <reggieduran15@gmail.com>
2023-06-22 22:13:46 +0800

    Added save and load using some erlang language

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -46,4 +46,18 @@ defmodule Cards do
     # Enum.member?(deck, hand)
     hand in deck
   end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+
+    case status do
+      :ok -> :erlang.binary_to_term binary
+      :error -> "That file does not exist"
+    end
+  end
 end
